e2847c2f8c428691c984158a41fa1f8b605aaa67
ANTONIO XOYOC BECERRA FARIAS <xoyoc_l2@hotmail.com>
2025-08-29 10:08:19 -0600

    Update settings.py

--- sistema/settings.py
+++ sistema/settings.py
@@ -29,8 +29,12 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = [
+    '.ondigitalocean.app',
+    '.digitaloceanspaces.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 # Application definition
 
